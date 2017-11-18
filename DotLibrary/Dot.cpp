c53b9ef1d9d4bed6832896337ea24355ed83a53c
Hampus Hallkivst <hampus@hallkvist.org>
2017-11-19 00:07:27 +0100

    Changes in Dot

--- DotLibrary/Dot.cpp
+++ DotLibrary/Dot.cpp
@@ -1,10 +1,14 @@
-#include "Dot.h"
-#include "Window.h"
+#include "dot.h"
 
-Dot::Dot(int xPos, int yPos) : _xPos(xPos), _yPos(yPos) {
-	rect = new Rect(20, 20, 20, 20, 255, 255, 255, 255);
+dot::dot(const std::string &image_path){
+	graphicalDot = new Rect(image_path);
 }
 
-Dot::~Dot() {
+
+dot::~dot(){
+
+}
+
+void dot::callDrawMethod() {
 
 }
--- DotLibrary/Dot.h
+++ DotLibrary/Dot.h
@@ -1,37 +1,15 @@
 #pragma once
 #include <string>
-#include <iostream>
 #include "rect.h"
-
 using namespace std;
 
-class Dot {
+class dot {
 public:
-	Dot(int xPos, int yPos);
-	~Dot();
-public:
-	int _xPos;
-	int _yPos;
-	
-	Dot *UP = nullptr;
-	Dot *DOWN = nullptr;
-	Dot *RIGHT = nullptr;
-	Dot *LEFT = nullptr;
-	Rect *rect = nullptr;
-
-	struct colors {
-		int r, g, b, a; // rect colors
-	};
-	struct position {
-		int x, y; // rect position relative to window
-	};
-	struct size {
-		int w, h; // rect size in pixels
-	};
-	struct rect_structure_format {
-		colors colors;
-		position position;
-		size size;
-	} rect_appearance;
-	
+	dot(const string &image_path);
+	~dot();
+private:
+	void callDrawMethod();
+private:
+	Rect *graphicalDot;
 };
+
