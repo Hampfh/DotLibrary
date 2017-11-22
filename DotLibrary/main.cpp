8aef5193750cf30f2c943914508cf5b161355667
Hampus Hallkivst <hampus@hallkvist.org>
2017-11-22 06:47:48 +0100

    Draged all .h files onto the same folder

--- DotLibrary/main.cpp
+++ DotLibrary/main.cpp
@@ -15,41 +15,13 @@ int main(int argc, char** argv) {
 
 	//FreeConsole();
 
-	Grid grid(3,3);
-	grid.SetupWindow("Test window", 1000, 750);
+	Grid grid(4,4);
+	grid.SetupWindow("Test window", 800, 500);
 
-	grid.GridSpecifications.betweenDotDistance = 40;
-	grid.GridSpecifications.dotSize = 20;
-	grid.GridSpecifications.gridOffset.x = 50;
-	grid.GridSpecifications.gridOffset.y = 50;
-
-	// set standard dot color
-	grid.GridSpecifications.color.r = 100;
-	grid.GridSpecifications.color.b = 70;
-	grid.GridSpecifications.color.g = 30;
+	
 
 	grid.drawGrid();
-
-	Dot* ThisDot = grid.specifyDot(1,3);
-	ThisDot->color.g = 6;
-	ThisDot->color.r = 2;
-	ThisDot->callDrawMethod();
 	grid.mainWindow->clear();
-	
-	while (true) {
-		pollEvents(*grid.mainWindow);
-
-		grid.drawGrid();
-		Dot* ThisDot = grid.specifyDot(2, 2);
-		ThisDot->color.g = 80;
-		ThisDot->color.r = 75;
-		ThisDot->callDrawMethod();
-		grid.mainWindow->clear();
-
-		if (grid.mainWindow->isClosed()) {
-			break;
-		}
-	}
-
+	while (true);
 	return 0;
 }
--- DotLibrary/window.cpp
+++ DotLibrary/window.cpp
@@ -1,7 +1,5 @@
 #include "window.h"
 #include <iostream>
-#include <SDL\SDL_image.h>
-#include <SDL\SDL_ttf.h>
 
 SDL_Renderer *Window::renderer = nullptr;
 
