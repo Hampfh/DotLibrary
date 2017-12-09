2efb761a3571ffa5d9b87753b61bb1b5bdc4453d
Hampus Hallkivst <hampus@hallkvist.org>
2017-12-09 06:27:00 +0100

    Added functions to represend all variables

--- DotLibrary/dot.h
+++ DotLibrary/dot.h
@@ -9,20 +9,29 @@ class Dot {
 private:
 	Dot(int xPos, int yPos);
 	~Dot();
+public:
 	void draw();
-	
+	// Get commands
+	inline colorTemplate getColor() { return(_color); };
+	inline sizeTemplate getSize() { return(_size); };
+	inline coordinateTemplate getCoords() { return(_coordinates); };
+	inline coordinateTemplate getScreenCoords() { return(_screenCoordinates); };
+	// Set commands
+	inline void setColor(int r, int g, int b) { _color.r = r, _color.g = g, _color.b = b; };
+	inline void setSize(int width, int height) { _size.w = width, _size.h = height; };
+
 private:
 	friend class Grid;
 	friend class Text;
+
+	colorTemplate _color;
+	sizeTemplate _size;
+	coordinateTemplate _coordinates;
+	coordinateTemplate _screenCoordinates;
 
 	Dot *UP = nullptr;
 	Dot *DOWN = nullptr;
 	Dot *RIGHT = nullptr;
 	Dot *LEFT = nullptr;
-
-	colorTemplate color;
-	sizeTemplate size;
-	coordinateTemplate coordinates;
-	coordinateTemplate screenCoordinates;
 };
 
