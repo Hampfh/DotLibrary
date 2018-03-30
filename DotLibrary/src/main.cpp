39bb0264f5a84e0725c51b32d6f9cd54b901b1e4
Hampus Hallkivst <hampus@hallkvist.org>
2018-03-30 14:36:15 +0200

    Moved all code to src folder

--- DotLibrary/src/main.cpp
+++ DotLibrary/src/main.cpp
@@ -10,12 +10,19 @@ void pollEvents(Window &win) {
 
 int main(int argv, char** argc) {
 	Grid gri(100, 100);
+	Grid gri2(100, 100);
 	Window myWin("Awsome window", 800, 800, DTL_RESIZABLE_WINDOW);
 	myWin.connectGrid(&gri);
+	myWin.connectGrid(&gri2);
 	gri.setSpacing(0);
 	gri.setDotSize(5);
 	gri.setColor(20, 20, 20);
 	gri.clear();
+	gri2.setOffset(5, 5);
+	gri2.setSpacing(0);
+	gri2.setDotSize(5);
+	gri2.setColor(50, 50, 50);
+	gri2.clear();
 
 	Text testText;
 
@@ -25,10 +32,8 @@ int main(int argv, char** argc) {
 		testText.drawText(gri.dot(2, 2), "12h50");
 
 		myWin.refresh();
-		if (myWin.isClosed()) {
-			break;
-		}
-		system("pause");
+		if (myWin.isClosed())
+			return 0;
 	}
-	return 1;
+	return 0;
 }
